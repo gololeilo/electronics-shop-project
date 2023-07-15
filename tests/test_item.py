e7201520cbc142211d3dec2049a75f988fb9e056
gololeilo <gololeilo@mail.ru>
2023-07-15 23:31:09 +1000

    ДЗ

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,20 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item():
+    return Item("Test Item", 10, 5)
+
+def test_init(item):
+    assert item.name == "Test Item"
+    assert item.price == 10
+    assert item.quantity == 5
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 50
+
+def test_apply_discount(item):
+    item.apply_discount()
+    assert item.price == 10
